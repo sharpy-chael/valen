7b9b7e4541e2667fb5f4233b732d71271c820ab8
yasmienmbanal <yasmienbanal0@gmail.com>
2026-02-09 16:45:49 +0800

    Add a meaningful message describing your changes

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const messages = [
+    "Are you sure?",
+    "Really Sure?",
+    "Are you positive?",
+    "Pookiee please...",
+    "Just think about it!",
+    "Please dont say no, I will be sad...",
+    "I will be very sad...",
+    "Pleaseeee dont say no!!!",
+    "Okay... I wont push you...",
+    "Just kidding, please say yes!<3"
+];
+const happyGif = "https://c.tenor.com/6foaT9raMTcAAAAj/bug-cat.gif";
+
+const gifs = [
+    "https://media.giphy.com/media/OPU6wzx8JrHna/giphy.gif",
+    "https://media.giphy.com/media/d2lcHJTG5Tscg/giphy.gif",
+    "https://media.giphy.com/media/ROF8OQvDmxytW/giphy.gif",
+    "https://media.giphy.com/media/ISOckXUybVfQ4/giphy.gif"
+];
+let originalYesSize = null;
+let messageIndex = 0;
+let gifIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    const gif = document.getElementById('valentineGif');
+
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+
+   if (!originalYesSize) {
+    originalYesSize = window.getComputedStyle(yesButton).fontSize;
+    }const currentSize = parseFloat(
+        window.getComputedStyle(yesButton).fontSize
+    );
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+
+    gif.src = gifs[gifIndex];
+    gifIndex = (gifIndex + 1) % gifs.length;
+}
+function handleYesClick() {
+    const gif = document.getElementById('valentineGif');
+    const title = document.querySelector('h1');
+    const yesButton = document.querySelector('.yes-button');
+    title.textContent = "Yayyy!! 💖 Happy Valentine’s Day!";
+   
+    gif.src = happyGif;
+    gifIndex = 0;
+    messageIndex = 0;
+    if (originalYesSize) {
+        yesButton.style.fontSize = originalYesSize;
+    }
+}
+
